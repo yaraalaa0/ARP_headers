00c18d41fc2ca72c878a19cef55bd38b46805fb0
programmatoroSeduto <francesc.ganci@gmail.com>
2021-01-23 13:01:24 -0800

    Added Handshake, version checking, more functionalities to the address table, deleted old stuff and other minor adjustments.

--- address-table/addresstable.c
+++ address-table/addresstable.c
@@ -1,32 +1,16 @@
-#include "addresstable/addresstable.h"
-#include "misc/random_index.h"
+#include "addresstable.h"
 
 // the IP address table
-int __remaining_ip_addr = __IP_TABLE_LEN;
 iptab_addr_t __ip_table[ __IP_TABLE_LEN ] = 
 {
-	{
-        "127.0.0.1",
-        __NOT_VISITED
-    },
-    {
-        "0.0.0.0",
-        __NOT_VISITED
-    },
-    {
-        "255.255.0.255",
-        __NOT_VISITED
-    },
+	{ "127.0.0.1", __NOD_AVAILABLE },
+    { "0.0.0.0", __NOD_AVAILABLE },
+    { "255.255.0.255", __NOD_AVAILABLE },
 };
 
-// clear the markers
-void iptab_clear()
-{
-    for( int i=0; i<__IP_TABLE_LEN; i++ )
-        __ip_table[i].visited = __NOT_VISITED;
-    
-    __remaining_ip_addr = __IP_TABLE_LEN;
-}
+// ------------------------
+//  BASIC OPERATIONS
+// ------------------------
 
 // get the total lenght of the table
 int iptab_len()
@@ -34,66 +18,98 @@ int iptab_len()
     return __IP_TABLE_LEN;
 }
 
-// are still unmarked nodes? return 1 if not all nodes are been visited
-int iptab_unvisited_nodes()
-{
-    return ( __remaining_ip_addr > 0 );
-}
-
-// mark a node as visited
-int iptab_mark(node_id n)
-{
-    if( __remaining_ip_addr == 0 )
-        return -1;
-    if( n >= __IP_TABLE_LEN || n < 0 )
-        return -1;
-
-    //mark the node
-    __ip_table[n].visited = __VISITED;
-    __remaining_ip_addr --;
-
-    return __remaining_ip_addr;
-}
-
-// check if a node is already marked
-int iptab_visited(node_id n)
-{
-    if( n >= __IP_TABLE_LEN || n < 0 )
-        return -1;
-    
-    return ( __ip_table[n].visited );
-}
-
 // get the IP address (as string) of a certain node
 char* iptab_getaddr(node_id n)
 {
-    if( n >= __IP_TABLE_LEN || n < 0 )
+    if( 
+        n >= __IP_TABLE_LEN || 
+        n < 0 || 
+        !__ip_table[n].ip_avail )
         return NULL;
     else
         return __ip_table[n].ip_addr;
 }
 
-// get randomly an unmarked node of the list
-node_id iptab_rand_idx()
+// get the ID of a certain node given its IP address
+// -1 is returned if such IP doesn't exist in the table (or it is not available)
+int iptab_get_ID_of(char* ip)
 {
-	node_id unvisited_nodes[__IP_TABLE_LEN];
-	int len_unv_nodes=0;
-	
-    if( __remaining_ip_addr == 0 ) return -1;
-	
-	/*
-		Generate a new table containing exclusively the node_id (so not the entire address)
-		of the unvisited nodes, in order to directly search for a random among them, without
-		the problem of reiterating in case of a random node already being visited.
-	*/
-	for(int i=0; i< __IP_TABLE_LEN; i++){
-		if (!__ip_table[i].visited){
-			unvisited_nodes[len_unv_nodes++] = (node_id)i;
-		}
-		
-	}
-	// We're already sure that there's at least one available node, since the check
-	// has been made in the beginning.
-    return unvisited_nodes[rdmindex(0, len_unv_nodes)];
+    int res = -1;
+    for( int i=0; i<__IP_TABLE_LEN; i++ )
+        if( strcasecmp( ip, __ip_table[i].ip_addr ) == 0 )
+        {
+            res = i;
+            break;
+        }
+    
+    if( (res < 0) || !iptab_is_available( res ) )
+        return -1;
 }
 
+// get the ID of the next available node in the table (in order)
+node_id iptab_get_next( node_id my )
+{
+    node_id n = my;
+    do  n = (n+1) % __IP_TABLE_LEN;  
+    while ( __ip_table[n].ip_avail == __NOD_NOT_AVAILABLE );
+    
+    return n;
+}
+
+// get the ID of the previous node in the table (in order)
+node_id iptab_get_prev( node_id my )
+{
+    node_id n = my;
+    do 
+    {
+        n--;
+        if( n < 0 ) n = __IP_TABLE_LEN - 1;
+    }
+    while ( __ip_table[n].ip_avail == __NOD_NOT_AVAILABLE );
+    
+    return n;
+}
+
+// ------------------------
+//  OPS ON "AVAILABLE" FLAG
+// ------------------------
+
+// check if a node is available (0 if it is not)
+int iptab_is_available(node_id n)
+{
+    return ( 
+        (n < __IP_TABLE_LEN) && 
+        (n >= 0) && 
+        __ip_table[n].ip_avail );
+}
+
+// mark a node as unavailable
+// 0 is returned if the operation is done
+int iptab_set_unavailable(node_id n)
+{
+    if( n < 0 || n >= __IP_TABLE_LEN )
+        return -1;
+    
+    __ip_table[n].ip_avail = __NOD_NOT_AVAILABLE;
+
+    return 0;
+}
+
+// set the node as available
+// 0 is returned if the operation is done
+int iptab_set_available(node_id n)
+{
+    if( n < 0 || n >= __IP_TABLE_LEN )
+        return -1;
+    
+    __ip_table[n].ip_avail = __NOD_AVAILABLE;
+
+    return 0;
+}
+
+// reset all the flags "available" as "yes" (default state)
+void iptab_reset_flags()
+{
+    for( int i=0; i<__IP_TABLE_LEN; i++ )
+        __ip_table[i].ip_avail = __NOD_AVAILABLE;
+}
--- address-table/addresstable.h
+++ address-table/addresstable.h
@@ -1,51 +1,67 @@
-#ifndef _ADDRESSTABLE_H_
-#define _ADDRESSTABLE_H_
+#ifndef _ADDRESSTABLE_SIMPLE_H_
+#define _ADDRESSTABLE_SIMPLE_H_
 
 #include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <stdint.h>
+#include <string.h>
+//include <limits.h>
 
 #define __IP_TABLE_LEN 3
-#define __RAND_IDX(max) (int) ((double) rand() * ( (double) max / (double) INT_MAX ))
 
-#define __VISITED 1
-#define __NOT_VISITED 0
+#define __NOD_AVAILABLE 1
+#define __NOD_NOT_AVAILABLE 0
 
-typedef node_id uint8_t; // essentially a char, 8bit without sign (0-255)
+typedef uint8_t node_id; // essentially a char, 8bit without sign (0-255)
+
 // the entry of the address table
 typedef struct iptab
 {
-    char ip_addr[16]; // 16 => 15+NULL terminating character
-    int visited;
+    char ip_addr[15];
+    short ip_avail;
+    // other infos?
 }
 iptab_addr_t;
 
+// ------------------------
+//  BASIC OPERATIONS
+// ------------------------
+
 // the table is an 'extern' array of (string) IP addresses
 extern iptab_addr_t __ip_table[ __IP_TABLE_LEN ];
-extern int __remaining_ip_addr;
-
-// clear the markers
-void iptab_clear();
 
 // get the total lenght of the table
 int iptab_len();
 
-// are still unmarked nodes? return how many nodes aren't marked
-int iptab_unvisited_nodes();
-
-// mark a node as visited; it returns the new number of remaining nodes or -1 on error
-int iptab_mark(node_id n);
-
-// check if a node is already marked; it returns -1 when the index isn't correct
-// return 1 is the node is marked, 0 if it is unmarked
-int iptab_visited(node_id n);
-
 // get the IP address (as string) of a certain node
-// return NULL if the address is wrong
+// return NULL is the address is wrong or the node is no longer available
 char* iptab_getaddr(node_id n);
 
-// get randomly an unmarked node of the list
-// it returns -1 if there aren't available nodes
-node_id iptab_rand_idx();
+// get the ID of a certain node given its IP address
+int iptab_get_ID_of(char* ip);
+
+// get the ID of the next available node in the table (in order)
+node_id iptab_get_next( node_id my );
+
+// get the ID of the previous node in the table (in order)
+node_id iptab_get_prev( node_id my );
+
+// ------------------------
+//  OPS ON "AVAILABLE" FLAG
+// ------------------------
+
+// check if a node is available (0 if it is not)
+int iptab_is_available(node_id n);
+
+// mark a node as unavailable
+// 0 is returned if the operation is done
+int iptab_set_unavailable(node_id n);
+
+// set the node as available
+// 0 is returned if the operation is done
+int iptab_set_available(node_id n);
+
+// reset all the flags "available" as "not" (default state)
+void iptab_reset_flags();
 
 #endif
