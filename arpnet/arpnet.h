e034d441ef17eb5c157a90b4ca54ed6052dbc132
hypothe <markeagle98@gmail.com>
2021-01-24 21:35:59 +0000

    fixed function behaviour, removing exit() and adding function documentation

--- arpnet/arpnet.h
+++ arpnet/arpnet.h
@@ -1,32 +1,73 @@
 #ifndef _ARPNET_H_
 #define _ARPNET_H_
 
-#include <time.h>
-#include <sys/time.h>
-#include <stdint.h>
-#include "/bitvector/bitvector.h"
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
 
-#define IP_TABLE_LEN 3
+/**********************************************//**
+* Initializing the main socket in a server code.
+*
+* Inside this function the socket gets generated,
+* a bind() is tried and, if succesful, is followed
+* by a listen().
+*
+* Arguments
+*	portno - Port number the calling process will
+*		attach the socket to;
+*
+* Return
+*	sockfd - the file descriptor of the opened
+*		and binded socket, if successful, otherwise
+*		the error code associated with the first
+*		error occurred in the process;
+**************************************************/
+int server_init(int portno);
 
-typedef node_id uint8_t; // essentially a char, 8bit without sign (0-255)
-// typedef ipv4_addr uint32_t;	// an ipv4 address is composed of 4 Bytes, aka 32 bits, (unused)
-/*
-	=== IP ADDRESS TABLE ===
-*/
-// the table is an 'extern' array
-extern char ip_table[IP_TABLE_LEN][15];
 
-/*
-	=== MESSAGE ===
-*/// format of the message shared with the nodes of the network
-typedef struct message
-{
-	node_id id;				/* the ID of the sender node */
-	struct timeval recvd;	/* time instant in which the sender received the previous message */
-	struct timeval sent;	/* time instant in which this message was sent */
-	node_id turnLeader;		/* the ID of the actual turn leader */
-	bitvector_t vis_set;	/* flags for visited nodes */
-}
-message_t;
+/**********************************************//**
+* Performing an accept on a connect request.
+*
+* Inside this function an accept() is performed,
+* and errors in such process checked.
+*
+* Arguments
+*	sockfd - the file descriptor of the socket the
+*		process is waiting of for a connection 
+*		request;
+*	p_cli_addr - a pointer to a struct where data
+*		of the client will be inserted;
+*
+* Return
+*	newsockfd - the file descriptor of the newly
+*		opened socket where the communication with
+*		the client can take place, if successful,
+*		otherwise the error code associated with
+*		the accept() failed call;
+**************************************************/
+int accept_client(int sockfd, struct sockaddr_in cli_addr);
+
+
+/**********************************************//**
+* Trying to connect to a server which IPv4 address
+* and open port number are known
+*
+* Inside this function a socket is opened and abort
+* connection with the server indicated is 
+* attempted.
+*
+* Arguments
+*	IPaddr - a string representing the name 
+*		associated with the server;
+*	portno - the port number the server is 
+*		supposed to be listening to;
+*
+* Return
+*	sockfd - the file descriptor of the opened
+*		socket where the communication with the
+*		desired server can take place;		
+**************************************************/
+int client_connection(char *IPaddr, int portno);
 
 #endif
