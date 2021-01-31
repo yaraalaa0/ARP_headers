b124f12517fc109b21e8b755cb63a64b906d5365
yaraalaa0 <37335420+yaraalaa0@users.noreply.github.com>
2021-01-31 22:44:25 +0200

    Update handshake.h

--- handshake/handshake.h
+++ handshake/handshake.h
@@ -6,6 +6,7 @@
 #include "../addresstable/addresstable.h"
 #include "version.h"
 
+// ID of the statring node of the handshake
 #define __HS_START_NODE_ID 0
 
 // the message to be shared during the handshake
@@ -15,37 +16,70 @@ typedef struct {
 }
 handshake_t;
 
+
+
 //--------------------------------------
 //  UTILS FOR STARTING HANDSHAKE
 //--------------------------------------
-
-// check if a node is the starting point for the handshake
+/**********************************************//**
+* Function to check if a node is the starting node for the handshake
+*
+* Arguments
+*	my_ip - the ip address of the node
+*
+* Return
+*       1 if the node is the starting one; otherwise, 0
+**************************************************/
 int hsh_imfirtst( char* my_ip );
 
-// initialize the handshake message
-// without setting as available the node which calls this function
+
+
+/**********************************************//**
+* Function to initialize the handshake message
+* sets the version to the current up-to-date version and initializes the available nodes bitvector
+*
+* Arguments
+*	hsm - pointer to the handshake message
+*
+**************************************************/
 void hsh_init( handshake_t* hsm );
+
+
+
+
 
 //--------------------------------------
 //  HANDSHAKE FIRST STEP
 //--------------------------------------
 
-/*
-    An unmarked node is considered as unavailable
-    Only the node that receives the message can mark itself as arrived.
-
-    The address table isn't updated until the second step. 
-*/
-
-// perform the checking of the version; if the version is correct, mark the node as available
-// the function returns 1 if the version is ok; 0 otherwise
+/**********************************************//**
+* Function to perform checking of the version of the node; 
+* if the version is correct, mark the node as available in the bitvector
+* An unmarked node is considered as unavailable
+* Only the node that receives the message can mark itself as available.
+*
+* Arguments
+*	my_id - the id of the current node
+*	hsm - pointer to the handshake message
+*   
+* Return
+*       1 if the version is ok; 0 otherwise
+**************************************************/
 int hsh_check_availability( node_id my_id, handshake_t* hsm );
+
+
+
 
 //--------------------------------------
 //  HANDSHAKE SECOND STEP
 //--------------------------------------
 
-// update the address table with the informations within the handshake message
+/**********************************************//**
+* Function to update the address table with the information of available and unavailable nodes contained in the handshake message
+*
+* Arguments
+*	hsm - pointer to the handshake message
+**************************************************/
 void hsh_update_iptab( handshake_t* hsm );
 
 #endif
